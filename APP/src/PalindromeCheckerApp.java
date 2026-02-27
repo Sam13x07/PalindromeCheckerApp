d2af38fbe49f7a8e1597031e1b2288b2199b8f1c
Sam13x07 <samlegionalive@gmail.com>
2026-02-27 15:38:41 +0530

    UC 2 Completed & Printed

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,8 +1,19 @@
-import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version 1.0");
-        System.out.println("System Initialized Successfully");
+        String original = "madam";
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println("The string \"" + original + "\" is a PALINDROME.");
+        } else {
+            System.out.println("The string \"" + original + "\" is NOT a palindrome.");
+        }
+
+        System.out.println("Original : " + original);
+        System.out.println("Reversed : " + reversed);
     }
 }
