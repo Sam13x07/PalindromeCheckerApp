4d4e0adebdd5f585f4ec55e3e1d5f86af8f20503
Sam13x07 <samlegionalive@gmail.com>
2026-03-16 14:58:06 +0530

    uc10

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -2,20 +2,16 @@ import java.util.Scanner;
 
 public class UseCase9PalindromeCheckerApp {
 
-    // Recursive method to check palindrome
     public static boolean isPalindrome(String str, int start, int end) {
 
-        // Base condition
         if (start >= end) {
             return true;
         }
 
-        // If characters are not equal
         if (str.charAt(start) != str.charAt(end)) {
             return false;
         }
 
-        // Recursive call
         return isPalindrome(str, start + 1, end - 1);
     }
 
